b769ff1b6e9de15f815af6a6bb67d09596240f59
JT Bertke <jbertke@uwyo.edu>
2025-04-28 19:15:11 -0600

    Fixed timing issues and reorganized the code

--- four_digit_counter.v
+++ four_digit_counter.v
@@ -7,15 +7,63 @@ module stopwatch_top(
     output [3:0] ms_msd, // Current milliseconds
     output [3:0] lap_ct_ms, lap_ctsecondslsd, lap_ct_secondsmsd, lap_ctminutes // Static timer values
 );
-    // Giant block of wires
-    wire tick_1ms; // Wire to connect the clock divider
-    wire bken1, bken2, bken3, bken4; // Wire to connect the backwards overflow
-    wire upen1, upen2, upen3, upen4; // Wire to connect the upwards overflow
-    wire msden1_reg, msden2_reg, msden3_reg; // Wire to connect the back/up overflow logic
+    
+    wire tick_1ms;
+    wire bken1, bken2, bken3, bken4; 
+    wire upen1, upen2, upen3, upen4; 
+    wire msden1_reg, msden2_reg, msden3_reg;
+    wire active_en = run && !kill;
 
-    // This is a synchronizer to store the stop/start button presses
-    // Reference the synchronizer in the mux to see how it works
-    reg [1:0] start_sync, stop_sync;
+    // input synchronization
+    reg [1:0] start_sync, stop_sync, lap_sync_ms;
+    wire start_press = start_sync[0] & ~start_sync[1];
+    wire stop_press = stop_sync[0] & ~stop_sync[1];
+    wire lap_press_ms = lap_sync_ms[0] & ~lap_sync_ms[1];
+
+    // Run control
+    reg run, kill;
+    always@(posedge clk or posedge rst or posedge clr)
+    begin
+        if(rst || clr)
+        begin
+            run <= 1'b0;
+            kill <= 1'b0;
+        end
+        else
+        begin
+            if (start_press)
+            begin
+                run <= 1'b1;
+                kill <= 1'b0;
+            end
+            else if(stop_press)
+            begin
+                run <= 1'b0;
+            end
+        end
+    end
+
+    // Kill logic
+    wire at_max = dir && ms_msd == 4'd9 && seconds_lsd == 4'd9 && seconds_msd == 4'd5 && minutes == 4'd9;
+    wire at_min = !dir && ms_msd == 4'd0 && seconds_lsd == 4'd0 && seconds_msd == 4'd0 && minutes == 4'd0;
+    always@(posedge clk or posedge rst or posedge clr)
+    begin
+        if(rst || clr || start_press || dir != dir_prev)
+        begin
+            kill <= 1'b0;
+        end
+        else if(run && (at_max || at_min))
+        begin
+            kill <= 1'b1;
+        end
+    end
+    
+    // Previous direction logic
+    reg dir_prev;
+    always @ (posedge clk)
+        dir_prev <= dir;
+
+    // Start/Stop press synchro
     always @ (posedge clk or posedge rst)
     begin
         if (rst)
@@ -30,37 +78,7 @@ module stopwatch_top(
         end
     end
 
-    // Stores the start/stop button press
-    wire start_press = start_sync[0] & ~start_sync[1];
-    wire stop_press = stop_sync[0] & ~stop_sync[1];
-
-    // This always block determines whether to run the counters or not
-    reg run;
-    always @ (posedge clk or posedge rst or posedge clr)
-    begin
-        // If rst or clr is pressed it stops the counters
-        if(rst || clr)
-        begin
-            run <= 1'b0;
-        end
-        // If the start is pressed it begins the counters
-        else if(start_press)
-        begin
-            run <= 1'b1;
-        end
-        // If stop is pressed it stops the counters
-        else if(stop_press)
-        begin
-            run <= 1'b0;
-        end
-    end
-
-    assign run_catch = run;
-    assign start_catch = start_press;
-
-    // This is the same synchronizer from the mux
-    reg [1:0] lap_sync_ms;
-    wire lap_press_ms;
+    // Lap Synchro
     always @ (posedge clk or posedge rst)
     begin
         if (rst) 
@@ -69,24 +87,18 @@ module stopwatch_top(
             lap_sync_ms <= {lap_sync_ms[0], lap};
     end
 
-    // Same logic from the mux
-    assign lap_press_ms = lap_sync_ms[0] & ~lap_sync_ms[1];
-    assign lap_press_ms_out = lap_press_ms;
-
-    wire start_repress = run & start_press;
-
-    // This is extremely long declaration of all our counters and
-    // the clock divider
+    // Clock Divider instantiation
     clock_divider_1ms div1(
         .clk(clk),
         .reset(rst),
         .tick_1ms(tick_1ms)
     );
-
+    // Millisecond Counter
     time_counter_1dig ms_counter(
         .en(active_en),
         .rst(rst),
         .clk(tick_1ms),
+        .clk100MHz(clk),
         .dir(dir),
         .clr(clr),
         .ct(ms_msd),
@@ -96,10 +108,12 @@ module stopwatch_top(
         .lap_ct(lap_ct_ms),
         .lap_press(lap_press_ms)
     );
+    // Second Counter
     time_counter_1dig seconds_lsd_counter(
         .en(msden1_reg),
         .rst(rst),
         .clk(tick_1ms),
+        .clk100MHz(clk),
         .dir(dir),
         .clr(clr),
         .ct(seconds_lsd),
@@ -109,10 +123,12 @@ module stopwatch_top(
         .lap_ct(lap_ctsecondslsd),
         .lap_press(lap_press_ms)
     );
+    // 10s Counter
     time_counter_1dig seconds_msd_counter(
         .en(msden2_reg),
         .rst(rst),
         .clk(tick_1ms),
+        .clk100MHz(clk),
         .dir(dir),
         .clr(clr),
         .ct(seconds_msd),
@@ -122,10 +138,12 @@ module stopwatch_top(
         .lap_ct(lap_ct_secondsmsd),
         .lap_press(lap_press_ms)
     );
+    // Minute Counter
     time_counter_1dig minutes_counter(
         .en(msden3_reg),
         .rst(rst),
         .clk(tick_1ms),
+        .clk100MHz(clk),
         .dir(dir),
         .clr(clr),
         .ct(minutes),
@@ -136,45 +154,14 @@ module stopwatch_top(
         .lap_press(lap_press_ms)
     );
 
-    // This is our overflow logic
-    // It just checks if the counters are running and whether there is an overflow
+    // Overflow logic
     assign msden1_reg = run && (bken1 || upen1);
     assign msden2_reg = run && (bken2 || upen2);
     assign msden3_reg = run && (bken3 || upen3);
-
-    // This is logic to determine whether or not the counters are at their minimum or maximum values
-    wire at_max = dir && ms_msd == 4'd9 && seconds_lsd == 4'd9 && seconds_msd == 4'd5 && minutes == 4'd9;
-    wire at_min = !dir && ms_msd == 4'd0 && seconds_lsd == 4'd0 && seconds_msd == 4'd0 && minutes == 4'd0;
-
-    // This block determines whether or not to kill the counters
-    // basically activates when all the counters reach their max/min values
-    wire active_en;
-    reg kill;
-    always@(posedge tick_1ms or posedge rst or posedge clr)
-    begin
-        if(rst || clr)
-        begin
-            kill <= 1'b0;
-        end
-        else if(run && (at_max || at_min))
-        begin
-            kill <= 1'b1;
-        end
-        else if(start_press)
-        begin
-            kill <= 1'b0;
-        end
-    end
     
-    // This is our active enable signal, it trickles down starting at the
-    // first counter, deactivates immediately when it hits the max or min or the kill activates
-    // and if it is running
-    assign active_en = run && !kill;
+    // Outputs to commmunicate with the Display Driver
+    assign run_catch = run;
+    assign start_catch = start_press;
+    assign lap_press_ms_out = lap_press_ms;
+
 endmodule
-
-
-
-        
-            
-
-    
