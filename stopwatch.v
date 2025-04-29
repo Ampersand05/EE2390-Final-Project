7443e6182576994170b4bbb5cbc9a407c173b0d5
JT Bertke <jbertke@uwyo.edu>
2025-04-29 17:37:23 -0600

    Update stopwatch.v

--- stopwatch.v
+++ stopwatch.v
@@ -1,14 +1,14 @@
 module stopwatch(
     input rst, clk, dir, clr, start, stop, lap, // Our basic outputs from the design specs
     output [3:0] an, // This is the anode signal driver from the multiplexer
-    output[6:0] seg // This is our output segment that is in sync with the anode select
+    output[0:6] seg // This is our output segment that is in sync with the anode select
 );
     // These are our wires to connect the outputs from the counters
     wire [3:0] mins, secs_lsd, secs_msd, ms;
     wire [3:0] lap_ct_minutes, lap_ct_seconds_lsd, lap_ct_seconds_msd, lap_ct_ms;
     wire [3:0] segNum; // These are used to transfrom the output from the mux into a 
                                 // raw output signal
-    wire [6:0] seg_out;
+    wire [0:6] seg_out;
 
     wire start_press, lap_press_ms; // This is a wire used to connect the stopwatch and the mux
 
@@ -52,7 +52,7 @@ module stopwatch(
         .start_press(start_press),
         .rst(rst),
         .run(run),
-        .seg(seg)
+        .seg(seg_out)
     );
 
     // Turning our segment output into a raw output
