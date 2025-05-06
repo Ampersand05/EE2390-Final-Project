b65b4b50964ba8a91a484204edb60e829268a059
JT Bertke <jbertke@uwyo.edu>
2025-05-06 16:47:02 -0600

    Update four_digit_counter.v

--- four_digit_counter.v
+++ four_digit_counter.v
@@ -1,26 +1,32 @@
 module stopwatch_top(
-    input rst, clk, dir, clr, start, stop, lap, // Similar inputs to the other modules
-    output run_catch, start_catch, lap_press_ms_out,
-    output [3:0] minutes, // Current minutes
-    output [3:0] seconds_msd, // Current seconds most significant digits
-    output [3:0] seconds_lsd, // Current seconds least significant digit
-    output [3:0] ms_msd, // Current milliseconds
-    output [3:0] lap_ct_ms, lap_ctsecondslsd, lap_ct_secondsmsd, lap_ctminutes // Static timer values
+    input rst, clk, dir, clr, start, stop, lap, TimeSet,
+    output run_catch, start_catch, lap_press_ms_out, flash,
+    output [3:0] minutes,
+    output [3:0] seconds_msd,
+    output [3:0] seconds_lsd,
+    output [3:0] ms_msd,
+    output [3:0] lap_ct_ms, lap_ctsecondslsd, lap_ct_secondsmsd, lap_ctminutes
 );
-    
     wire tick_1ms;
-    wire bken1, bken2, bken3, bken4; 
-    wire upen1, upen2, upen3, upen4; 
+    wire bken1, bken2, bken3, bken4;
+    wire upen1, upen2, upen3, upen4;
     wire msden1_reg, msden2_reg, msden3_reg;
-    wire active_en = run && !kill;
 
-    // input synchronization
+    // TimeSet mode control
+    reg time_set_mode;
+    reg [1:0] time_sync;
+    wire time_press = time_sync[0] & ~time_sync[1];
+
+    // Input synchronization
     reg [1:0] start_sync, stop_sync, lap_sync_ms;
     wire start_press = start_sync[0] & ~start_sync[1];
     wire stop_press = stop_sync[0] & ~stop_sync[1];
-    wire lap_press_ms = lap_sync_ms[0] & ~lap_sync_ms[1];
+    wire lap_press_ms = lap_sync_ms[0] & ~lap_sync[1];
 
-    // New flashing display control
+    // Active enable for counters
+    wire active_en = (run || (time_set_mode && start_press)) && !kill;
+
+    // Flashing display control
     reg flash;
     always @(posedge clk or posedge rst or posedge clr)
     begin
@@ -38,21 +44,33 @@ module stopwatch_top(
 
     // Run control
     reg run, kill;
-    always@(posedge clk or posedge rst or posedge clr)
+    always @(posedge clk or posedge rst or posedge clr)
     begin
-        if(rst || clr)
+        if (rst || clr)
         begin
             run <= 1'b0;
             kill <= 1'b0;
+            time_set_mode <= 1'b0;
         end
         else
         begin
-            if (start_press)
+            // TimeSet mode toggle
+            if (time_press && !time_set_mode)
+                time_set_mode <= 1'b1;
+            else if (time_press && time_set_mode)
+                time_set_mode <= 1'b0;
+
+            // Run logic
+            if (time_set_mode)
+            begin
+                run <= 1'b0; // Normal run disabled in TimeSet mode
+            end
+            else if (start_press)
             begin
                 run <= 1'b1;
                 kill <= 1'b0;
             end
-            else if(stop_press)
+            else if (stop_press)
             begin
                 run <= 1'b0;
             end
@@ -62,30 +80,30 @@ module stopwatch_top(
     // Kill logic
     wire at_max = dir && ms_msd == 4'd9 && seconds_lsd == 4'd9 && seconds_msd == 4'd5 && minutes == 4'd9;
     wire at_min = !dir && ms_msd == 4'd0 && seconds_lsd == 4'd0 && seconds_msd == 4'd0 && minutes == 4'd0;
-    always@(posedge clk or posedge rst or posedge clr)
+    always @(posedge clk or posedge rst or posedge clr)
     begin
-        if(rst || clr || start_press || stop_press || dir != dir_prev)
+        if (rst || clr || start_press || stop_press || dir != dir_prev)
         begin
             kill <= 1'b0;
         end
-        else if(run && (at_max || at_min))
+        else if ((run || time_set_mode) && (at_max || at_min))
         begin
             kill <= 1'b1;
         end
     end
-    
+
     // Previous direction logic
     reg dir_prev;
-    always @ (posedge clk or posedge rst)
+    always @(posedge clk or posedge rst)
     begin
-        if(rst)
+        if (rst)
             dir_prev <= dir;
-        else if(!run)
+        else if (!run && !time_set_mode)
             dir_prev <= dir;
     end
 
-    // Start/Stop press synchro
-    always @ (posedge clk or posedge rst)
+    // Start/Stop press synchronization
+    always @(posedge clk or posedge rst)
     begin
         if (rst)
         begin
@@ -99,10 +117,23 @@ module stopwatch_top(
         end
     end
 
-    // Lap Synchro
-    always @ (posedge clk or posedge rst)
+    // TimeSet synchronization
+    always @(posedge clk or posedge rst or posedge clr)
     begin
-        if (rst) 
+        if (rst || clr)
+        begin
+            time_sync <= 2'b00;
+        end
+        else
+        begin
+            time_sync <= {time_sync[0], TimeSet};
+        end
+    end
+
+    // Lap synchronization
+    always @(posedge clk or posedge rst)
+    begin
+        if (rst)
             lap_sync_ms <= 2'b00;
         else
             lap_sync_ms <= {lap_sync_ms[0], lap && dir};
@@ -112,8 +143,10 @@ module stopwatch_top(
     clock_divider_1ms div1(
         .clk(clk),
         .reset(rst),
-        .tick_1ms(tick_1ms)
+        .tick_1ms(tick_1ms),
+        .TimeSet(time_set_mode)
     );
+
     // Millisecond Counter
     time_counter_1dig ms_counter(
         .en(active_en),
@@ -129,6 +162,7 @@ module stopwatch_top(
         .lap_ct(lap_ct_ms),
         .lap_press(lap_press_ms)
     );
+
     // Second Counter
     time_counter_1dig seconds_lsd_counter(
         .en(msden1_reg),
@@ -144,6 +178,7 @@ module stopwatch_top(
         .lap_ct(lap_ctsecondslsd),
         .lap_press(lap_press_ms)
     );
+
     // 10s Counter
     time_counter_1dig seconds_msd_counter(
         .en(msden2_reg),
@@ -159,6 +194,7 @@ module stopwatch_top(
         .lap_ct(lap_ct_secondsmsd),
         .lap_press(lap_press_ms)
     );
+
     // Minute Counter
     time_counter_1dig minutes_counter(
         .en(msden3_reg),
@@ -176,11 +212,11 @@ module stopwatch_top(
     );
 
     // Overflow logic
-    assign msden1_reg = run && (bken1 || upen1);
-    assign msden2_reg = run && (bken2 || upen2);
-    assign msden3_reg = run && (bken3 || upen3);
-    
-    // Outputs to commmunicate with the Display Driver
+    assign msden1_reg = (run || (time_set_mode && start_press)) && (bken1 || upen1);
+    assign msden2_reg = (run || (time_set_mode && start_press)) && (bken2 || upen2);
+    assign msden3_reg = (run || (time_set_mode && start_press)) && (bken3 || upen3);
+
+    // Outputs to communicate with the Display Driver
     assign run_catch = run;
     assign start_catch = start_press;
     assign lap_press_ms_out = lap_press_ms;
